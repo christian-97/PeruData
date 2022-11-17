38635c74defed662ddc5f60561e4c47f684d58d3
christian-97 <christhiangutierrezrosas@gmail.com>
2022-11-17 14:38:31 -0500

    Initial commit

---
+++ app/src/main/java/pe/edu/utp/perdata/Splash.java
@@ -0,0 +1,45 @@
+package pe.edu.utp.perdata;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+import android.view.WindowManager;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+public class Splash extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        //getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,WindowManager.LayoutParams.FLAG_FULLSCREEN);
+        setContentView(R.layout.activity_main);
+
+        //animaciones
+
+        Animation animacion1 = AnimationUtils.loadAnimation(this, R.anim.desplazamiento_arriba);
+        Animation animacion2 = AnimationUtils.loadAnimation(this, R.anim.desplazamiento_abajo);
+
+        TextView fromTextView = findViewById(R.id.fromTextView);
+        TextView peruDataTextView = findViewById(R.id.peruDataTextView);
+        ImageView logoImageView = findViewById(R.id.logoImageView);
+
+        fromTextView.setAnimation(animacion2);
+        peruDataTextView.setAnimation(animacion2);
+        logoImageView.setAnimation(animacion1);
+
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                Intent intent = new Intent(Splash.this, LoginActivity.class);
+                startActivity(intent);
+                finish();
+            }
+        }, 4000);
+
+    }
+}
